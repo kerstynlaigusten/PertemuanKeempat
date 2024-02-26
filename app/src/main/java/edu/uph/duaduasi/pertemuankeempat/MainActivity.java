61c895fba550680d493e746400eb9e521fd66557
kerstynlaigusten <kerstynlaigusten@gmail.com>
2024-02-26 17:20:33 +0700

    Initial commit

---
+++ app/src/main/java/edu/uph/duaduasi/pertemuankeempat/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.uph.duaduasi.pertemuankeempat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
